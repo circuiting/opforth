3d47a7f625d5a2bdecb3811625cea2f2db8f5ee8
carltonhimes <mail@carltonhimes.com>
2025-04-01 14:02:13 -0700

    Added license notices and README info

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-this is a test
+Opforth README
+
+Opforth is a project under development that aims to be an
+implementation of the ANS Forth language.
+
+Author: Carlton Himes <mail@opforth.com>
+
+Copyright © 2025 Carlton Himes
+
+This file is part of Opforth.
+
+Opforth is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, either version
+3 of the License, or (at your option) any later version.
+
+Opforth is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public
+License along with Opforth. If not, see
+<https://www.gnu.org/licenses/>.
--- opforth.fs
+++ opforth.fs
@@ -1,4 +1,26 @@
-\ Opforth Core and Core Extension Words
+\ opforth.fs
+
+\ Copyright © 2025 Carlton Himes
+
+\ This file is part of Opforth.
+
+\ Opforth is free software: you can redistribute it and/or
+\ modify it under the terms of the GNU General Public Li-
+\ cense as published by the Free Software Foundation, either
+\ version 3 of the License, or (at your option) any later
+\ version.
+
+\ Opforth is distributed in the hope that it will be useful,
+\ but WITHOUT ANY WARRANTY; without even the implied warran-
+\ ty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+\ See the GNU General Public License for more details.
+
+\ You should have received a copy of the GNU General Public
+\ License along with Opforth. If not, see
+\ <https://www.gnu.org/licenses/>.
+
+
+\ Contents
 
 
 \ Core Stack (13)
@@ -289,3 +311,23 @@
 \ abort
 \ abort"
 \ evaluate
+
+
+\ Core and Core Extension Words
+
+
+\ Core Stack
+
+\ drop
+\ dup
+\ swap
+\ over
+\ rot
+\ ?dup
+\ 2drop
+\ 2dup
+\ 2swap
+\ 2over
+\ >r
+\ r>
+\ r@
