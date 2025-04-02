aa41a5d99972a367de3f96fd4cc331331d92c3dd
carltonhimes <mail@carltonhimes.com>
2025-04-02 15:23:57 -0700

    Draft descriptions for number test, bitwise logic, address math, and memory words

--- opforth.fs
+++ opforth.fs
@@ -68,7 +68,7 @@
 \ /mod      n1 n2 -- n3 n4
 \ sm/rem    d n1 -- n2 n3
 \ fm/mod    d n1 -- n2 n3
-\ um/mod    d n1 -- n2 n3
+\ um/mod    ud u1 -- u2 u3
 \ */        n1 n2 n3 -- n4
 \ */mod     n1 n2 n3 -- n4 n5
 
@@ -126,7 +126,7 @@
 
 \ @       a-addr -- x
 \ !       x a-addr --
-\ c@      c-addr -- x
+\ c@      c-addr -- char
 \ c!      char c-addr --
 \ 2@      a-addr -- x1 x2
 \ 2!      x1 x2 a-addr --
@@ -137,7 +137,7 @@
 
 \ Core-Ext Memory
 
-\ erase    c-addr u -- false | i*x true
+\ erase    addr u --
 \ pad      -- c-addr
 
 
@@ -377,21 +377,21 @@ $0005 opcode ?dup  ( x -- x x | 0 )
 \ Put a copy of cell pair x1 x2 on top of the stack.
 
 
-$0006 opcode >r  ( Exe: x R: -- R:x )
+$0006 opcode >r  ( Compi: -- ) ( Exe: x R: -- R:x )
 
 \ Interpretation: Undefined
 \ Execution: Transfer the top data stack item to the return
 \ stack.
 
 
-$0007 opcode r>  ( Exe: R:x -- x R: )
+$0007 opcode r>  ( Compi: -- ) ( Exe: R:x -- x R: )
 
 \ Interpretation: Undefined
 \ Execution: Transfer the top return stack item to the data
 \ stack.
 
 
-$0008 opcode r@  ( Exe: R:x -- x R:x )
+$0008 opcode r@  ( Compi: -- ) ( Exe: R:x -- x R:x )
 
 \ Interpretation: Undefined
 \ Execution: Put a copy of the top return stack item onto the
@@ -427,21 +427,23 @@ $000a opcode tuck  ( x1 x2 -- x2 x1 x2 )
 \ top.
 
 
-: 2>r  ( Exe: x1 x2 R: -- R:x1 R:x2 )  swap >r >r ;
+: 2>r  ( Compi: -- ) ( Exe: x1 x2 R: -- R:x1 R:x2 )
+  swap >r >r ;
 
 \ Interpretation: Undefined
 \ Execution: Transfer the cell pair on top of the data stack to
 \ the return stack.
 
 
-: 2r>  ( Exe: R:x1 R:x2 -- x1 x2 R: )  r> r> swap ;
+: 2r>  ( Compi: -- ) ( Exe: R:x1 R:x2 -- x1 x2 R: )
+  r> r> swap ;
 
 \ Interpretation: Undefined
 \ Execution: Transfer the cell pair on top of the return stack
 \ to the data stack.
 
 
-: 2r@  ( Exe: R:x1 R:x2 -- x1 x2 R:x1 R:x2 )
+: 2r@  ( Compi: -- ) ( Exe: R:x1 R:x2 -- x1 x2 R:x1 R:x2 )
   r> r> 2dup >r >r swap ;
 
 \ Interpretation: Undefined
@@ -465,12 +467,12 @@ $000c opcode -  ( nu1 nu2 -- nu3 )
 
 $000d opcode 1+  ( nu1 -- nu2 )
 
-\ nu2 is nu1 incremented by one.
+\ nu2 is the result of incrementing nu1 by one.
 
 
 $000e opcode 1-  ( nu1 -- nu2 )
 
-\ nu2 is nu1 decremented by one.
+\ nu2 is the result of decrementing nu1 by one.
 
 
 $000f opcode negate  ( n1 -- n2 )
@@ -604,6 +606,236 @@ $0015 opcode u<  ( u1 u2 -- flag )
 \ If u1 is less than u2, flag is true. Otherwise flag is false.
 
 
+: max  ( n1 n2 -- n3 )  something ;
+
+\ Compare the top two integers on the stack. n3 is the integer
+\ that is greater (closer to positive infinity).
+
+
+: min  ( n1 n2 -- n3 )  something ;
+
+\ Compare the top two integers on the stack. n3 is the integer
+\ that is lesser (closer to negative infinity).
+
+
+
+\ Core-ext Number Test
+
+
+$0016 opcode 0<>  ( x -- flag )
+
+\ If all bits of x are zero, flag is false. Otherwise flag is
+\ true.
+
+
+$0017 opcode 0>  ( n -- flag )
+
+\ If n is greater than zero, flag is true. Otherwise flag is
+\ false.
+
+
+$0018 opcode <>  ( x1 x2 -- flag )
+
+\ If x1 is bit-for-bit the same as x2, flag is false. Otherwise
+\ flag is true.
+
+
+$0019 opcode u>  ( u1 u2 -- flag )
+
+\ If u1 is greater than u2, flag is true. Otherwise flag is
+\ false.
+
+
 : within  ( nu1 nu2 nu3 -- flag )  something ;
 
 \ Description goes here
+
+
+
+\ Core Bitwise Logic
+
+
+$001a opcode invert  ( x1 -- x2 )
+
+\ x2 is the result of inverting all bits of x1.
+
+
+$001b opcode and  ( x1 x2 -- x3 )
+
+\ x3 is the bit-for-bit logical AND of x1 with x2.
+
+
+$001c opcode or  ( x1 x2 -- x3 )
+
+\ x3 is the bit-for-bit logical OR of x1 with x2.
+
+
+$001d opcode xor  ( x1 x2 -- x3 )
+
+\ x3 is the bit-for-bit logical XOR of x1 with x2.
+
+
+$001c opcode 2*  ( x1 -- x2 )
+
+\ x2 is the result of shifting all bits of x1 to the left by one
+\ binary digit. The vacated least significant bit becomes zero.
+
+
+$001d opcode 2/  ( x1 -- x2 )
+
+\ x2 is the result of shifting all bits of x1 to the right by
+\ one binary digit. The vacated most significant bit is un-
+\ changed.
+
+
+: lshift  ( x1 u -- x2 )  something ;
+
+\ x2 is the result of shifting all bits of x1 to the left by u
+\ binary digits. The vacated least significant bits become ze-
+\ roes.
+
+
+: rshift  ( x1 u -- x2 )  something ;
+
+\ x2 is the result of shifting all bits of x1 to the right by u
+\ binary digits. The vacated most significant bits are unchanged.
+
+
+
+\ Core-Ext Bitwise Logic
+
+
+$001e opcode true  ( -- true )
+
+\ Put a logical TRUE flag on the stack. All bits of the flag are
+\ ones.
+
+
+$001f opcode false  ( -- false )
+
+\ Put a logical FALSE flag on the stack. All bits of the flag
+\ are zeroes.
+
+
+
+\ Core Address Math
+
+
+: cells  ( n1 -- n2 )  ; immediate
+
+\ n2 is the size in address units of n1 cells. Because the size
+\ of an Opforth cell is one address unit, n2 is equal to n1.
+
+
+: chars  ( n1 -- n2 )  ; immediate
+
+\ n2 is the size in address units of n1 character. Because the
+\ size of an Opforth character is one address unit, n2 is equal
+\ to n1.
+
+
+synonym cell+ 1+  ( a-addr1 -- a-addr2 )
+
+\ a-addr2 is the result of incrementing a-addr1 by one address
+\ unit. Because the size of an Opforth cell is one address unit,
+\ this operation is equivalent to 1+.
+
+
+synonym char+ 1+  ( c-addr1 -- c-addr2 )
+
+\ a-addr2 is the result of incrementing c-addr1 by one address
+\ unit. Because the size of an Opforth character is one address
+\ unit, this operation is equivalent to 1+.
+
+
+: aligned  ( addr -- a-addr )  ; immediate
+
+\ a-addr is the first aligned address greater than or equal to
+\ addr. Because the size of an Opforth cell is one address unit,
+\ a-addr is equal to addr.
+
+
+: count  ( c-addr1 -- c-addr2 u )  something ;
+
+\ Given a counted string with the count character located at
+\ c-addr1, return the non-counted string representation. c-addr2
+\ is the location of the first character after the count charac-
+\ ter, and u is the string length excluding the count character.
+
+
+
+\ Core Memory
+
+
+$0020 opcode @  ( a-addr -- x )
+
+\ Read the cell located at memory address a-addr and put the
+\ cell on the stack.
+
+
+: !  ( x a-addr -- )  tuck! drop ;
+
+\ Write x to memory address a-addr.
+
+
+synonym c@ @  ( c-addr -- char )
+
+\ Read the character located at memory address c-addr and put
+\ the character on the stack.
+
+
+synonym c! !  ( char c-addr -- )
+
+\ Write char to memory address c-addr.
+
+
+: 2@  ( a-addr -- x1 x2 )  dup cell+ @ swap @ ;
+
+\ Read the cell pair located at memory address a-addr and put
+\ the cell pair on the stack. x2 is the cell at a-addr and x1
+\ is the next consecutive cell.
+
+
+: 2!  ( x1 x2 a-addr -- )  tuck ! cell+ ! ;
+
+\ Write the cell pair x1 x2 to memory address a-addr. x2 is
+\ written to a-addr and x1 is written to the next consecutive
+\ cell.
+
+
+: +!  ( nu a-addr -- )  tuck @ + swap ! ;
+
+\ Read the single-cell integer located at memory address a-addr,
+\ add nu to the integer, and write the result to the same ad-
+\ dress.
+
+
+: move  ( addr1 addr2 u -- )  something ;
+
+\ If u is greater than zero, copy the contents of u consecutive
+\ address units at addr1 to the u consecutive address units at
+\ addr2. After MOVE completes, the u consecutive address units
+\ at addr2 contain exactly what the u consecutive address units
+\ at addr1 contained before the move.
+
+
+: fill  ( c-addr u char -- )  something ;
+
+\ If u is greater than zero, store char in each of u consecutive
+\ characters of memory beginning at c-addr.
+
+
+
+\ Core-Ext Memory
+
+
+: erase  ( addr u -- )  something ;
+
+\ If u is greater than zero, clear all bits in each of u consec-
+\ utive address units of memory beginning at addr.
+
+
+: pad  ( -- c-addr )  something ;
+
+\ c-addr is the address of a transient region that can be used
+\ to hold data for intermediate processing.
